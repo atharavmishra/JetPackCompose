c3a2d7ee186e734f6932791975c7abc74acd6f00
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2023-01-19 01:12:30 +0530

    Committing project

--- app/src/main/java/android/myapplication/jetpackcompose/MainActivity.kt
+++ app/src/main/java/android/myapplication/jetpackcompose/MainActivity.kt
@@ -3,7 +3,6 @@ package android.myapplication.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,10 +10,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.myapplication.jetpackcompose.ui.theme.JetpackComposeTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import java.security.AllPermission
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,20 +39,25 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 @Composable
-fun Customtext(text1 : String){
-    Text(text = text1)
+fun Customtext(){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Blue), contentAlignment = Alignment.TopCenter, ){
+        Box(modifier = Modifier.background(Color.Red)){
+            Text(text = "Atharv")
+        }
+        
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Column(modifier = Modifier.fillMaxSize() ) {
-            androidx.compose.material.Surface(modifier = Modifier.width(200.dp).height(200.dp),color = MaterialTheme.colors.primary) {
-                
+
+                Greeting("Android")
+        Customtext()
             }
-            Greeting("Android")
-            Customtext(text1 = "Hello world")
-        }
+
+
     }
-}
